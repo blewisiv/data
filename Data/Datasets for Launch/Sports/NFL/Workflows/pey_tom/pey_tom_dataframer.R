7636bdaa90b3fbc48491574fd4b95c2e64c63b57
Alex Bresler <alex@aragorntech.com>
2013-10-15 11:06:02 -0400

    NFL Viz Updates

--- Data/Datasets for Launch/Sports/NFL/Workflows/pey_tom/pey_tom_dataframer.R
+++ Data/Datasets for Launch/Sports/NFL/Workflows/pey_tom/pey_tom_dataframer.R
@@ -11,7 +11,7 @@ brees <- data.frame(read.csv(list.files()[3]),stringsAsFactors=F)
 pey <- data.frame(read.csv(list.files()[4]),stringsAsFactors=F)
 tom <- data.frame(read.csv(list.files()[5]),stringsAsFactors=F)
 
-game_number <- 5 #ENTER GAME NUMBER
+game_number <- 6 #ENTER GAME NUMBER
 
 #peyton
 pey_2013_progression <- subset(pey, season == 2013 , season_game <= game_number)
@@ -46,6 +46,9 @@ merged_df <- data.table(merge(aaron_2011_progression[,cols],merged_df,all.x=T,al
 merged_df <- data.table(merge(marino_1984_progression[,cols],merged_df,all.x=T,all.y=T))
 
 names(merged_df)
+
+
+
 ds <-data.frame( 
 	merged_df[, list( completions = sum(pass_completions), attempts = sum(pass_attempts), passing_yards = sum(passing_yards), passing_touchdowns = sum(passing_touchdowns), interceptions = sum(interceptions_thrown), rushing_touchdowns = sum(rushing_touchdowns), qb_rating = mean(qb_rating), team_points = sum(team_score) , opponent_points = sum(opponent_score)
 		), 
@@ -64,15 +67,29 @@ ds$touchdowns_per_interception <- round(passing_touchdowns/interceptions,digits=
 ds$touchdowns_per_attempt <- 100*round(passing_touchdowns/attempts,digits=4)
 ds$touchdowns_per_completion <-100*round(passing_touchdowns/completions,digits=4)
 ds$player_point_to_opponent_point_ratio <- 100*round(((passing_touchdowns+rushing_touchdowns)*6)/opponent_points,digits=4)
-
+ds$qb_rating <- round(ds$qb_rating,digits=2)
 
 ds <- merge(ds,outcomes)
 
 ds <- ds[order(ds$passing_touchdowns,decreasing=T),]
 ds[,c('player','season')]
+ds$image_url <- c('http://vlsportysexycool.com/wp-content/uploads/2012/03/Peyton-Manning-Broncos.png','http://3.bp.blogspot.com/-V_1dP__CU00/UjM6e5f6_XI/AAAAAAAAKTY/hth-u46579o/s1600/Brady_03.PNG','http://thegothamgridiron.files.wordpress.com/2010/08/young-dan-marino.png','http://www.wallpaperati.com/wp-content/uploads/2011/01/aaron-rodgers-wallpaper-3.png','http://www.coltsdigital.me/community/uploads/gallery/album_3/gallery_1_3_109440.png','http://maizeandgoblue.com/wp-content/uploads/2011/09/Tom-Brady.png','http://vlsportysexycool.com/wp-content/uploads/2012/06/Drew-Brees.png')
 ds$label <- c("Manning '13","Brady '07","Marino '84","Rodgers '11","Manning '04","Brady '11","Brees '11")
-names(ds)
-final_ds <- ds[,c(1:2,6,9,5:3,7,12,10:11,21:22,13:20,23)]
-names(final_ds)
+ds$week <- game_number
+	ds$detail <- sprintf("<table cellpadding='3' style='line-height:1.25'><tr><th colspan='2.5'>%1$s</th></tr><tr><td><img src='%2$s' height='125' width='100'></td><td align='left'>Season: %3$s<br>Week: %7$s<br>QB Rating: %5$s<br>Passing Touchdowns: %4$s<br>Passing Yards: %6$s<br>Interceptions: %8$s</td></tr></table>",
+	ds$label,
+	ds$image_url,
+	ds$season,
+	ds$passing_touchdowns,
+	ds$qb_rating,
+	ds$passing_yards,
+	ds$week,
+	ds$interceptions)
+
+
+
+final_ds <- ds[,c(1:2,6,9,5:3,7,12,10:11,21:22,13:20,24,26)]
+
+
 setwd("~/Desktop/Github/Aragorn/data/Data/Datasets for Launch/Sports/NFL/Workflows/pey_tom/data/weekly_viz_data")
 write.csv(final_ds,paste('Week_',game_number,'_Peyton_Quest','.csv',sep=""))
--- Data/Datasets for Launch/Sports/NFL/Workflows/pey_tom/pey_tom_visualizer.R
+++ Data/Datasets for Launch/Sports/NFL/Workflows/pey_tom/pey_tom_visualizer.R
@@ -2,25 +2,33 @@
 library(rCharts)
 
 setwd("~/Desktop/Github/Aragorn/data/Data/Datasets for Launch/Sports/NFL/Workflows/pey_tom/data/weekly_viz_data")
-list.files()[2]
-ds <- read.csv(list.files()[2])
-
-p1 <- nPlot(passing_touchdowns ~ label, group = 'player', data = ds, type = 'multiBarChart')
+list.files()
+ds <- read.csv(list.files()[3])
+ds$X <- NULL
+color=c('#DF6108','#0D254C','#005E6A','#313F36','#163F83','#C0001D','#C9B074')
+label <- c("Manning '13","Brady '07","Marino '84","Rodgers '11","Manning '04","Brady '11","Brees '11")
+players_colors <- data.frame(cbind(label,color),stringsAsFactors=F)
+players_colors$player <- c('Peyton Manning','Tom Brady', 'Dan Marino', 'Aaron Rodgers', 'Peyton Manning','Tom Brady','Drew Brees')
+players_colors$season <- c(2013,2007,1984,2011,2004,2011,2011)
+t <- cbind(players_colors,ds)
+t <- t[ , -c(5,6,26)]
+names(t)
+t <- t[order(t$passing_touchdowns,decreasing=T),]
+p1 <- nPlot(passing_touchdowns ~ player, group = 'label', data = t, type = 'multiBarChart')
 p1$chart(showControls = F)
-p1$chart(color=c('#DF6108','#0D254C','#005E6A','#313F36','#C9B074'))
-p1
-p1$addControls("y", value = "passing_touchdowns", values = names(ds)[c(4:22)])
-p1$addControls("group", value = "passing_touchdowns", values = names(ds)[c(2,3,13,14)])
+colors <- as.character(ds$color)
+p1$chart(color=color)
+names(t)
+p1$addControls("y", value = "passing_touchdowns", values = names(t)[c(5:23)])
+p1$addControls("x", value = "player", values = names(t)[c(3,6:14,4,15:23,5,6)])
 p1$yAxis( tickFormat="#!function(d) {return d3.format('.02f')(d)}!#" )
-
+p1
 p1$chart(tooltip = "#! function(key, x, y, e, graph) {
-  return '<h3><b>' + e.point.player + '</b></h3>' +
-    '<p><b>' + y + '</b></p>'
-'<p><b>' + 'After 5 Games' + '</b></p>'
+  return '<p>' + e.point.detail + '</p>' +
+    '<p>' + ''  + '</p>'
 }!#")
 p1
 
-
-p1$publish('Peyton Quest Week 5', host = 'gist')
+	p1$publish('Peyton Quest Week 6', host = 'gist')
 
 
